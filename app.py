90d0a848d82bcf27f7fdbc265186a79a2905f396
Nitish Yadav <31303415+NH1922@users.noreply.github.com>
2019-04-10 23:26:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from calculator import total,product
+from calculator import total,product,difference
 
 print("The sum is :",total(2,3))
 print("The product is:",product(2,3))
-
-
+print("The difference is :",difference(2,3))
